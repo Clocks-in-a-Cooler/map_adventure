17c97622ee912419c0ad398df3b76c0603564335
clocks-in-a-cooler <>
2018-07-14 21:40:18 -0400

    made the messages pane work. you're welcome. it took a disk load's worth of work to get it right.

--- javascript/engine.js
+++ javascript/engine.js
@@ -11,11 +11,28 @@ var Engine = {
         return btoa(JSON.stringify(game));
     },
 
+    notify: function(msg) {
+        if (message_panel.childNodes.length > 9 ) {
+            message_panel.removeChild(message_panel.childNodes[9]);
+        }
+
+        //so much complicated code!
+        var new_message = document.createElement("DIV");
+        var m_att = document.createAttribute("class");
+        m_att.value = "message";
+        new_message.setAttributeNode(m_att);
+
+        var message_text = document.createTextNode(msg);
+        new_message.appendChild(message_text);
+
+        message_panel.insertBefore(new_message, message_panel.childNodes[0]);
+    },
+
     load_save_code: function(save_code) {
         try {
             return JSON.parse(atob(save_code));
         } catch (err) {
             return null;
         }
-    }
+    },
 }
