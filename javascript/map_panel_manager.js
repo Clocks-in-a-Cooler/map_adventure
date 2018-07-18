6ef228b101ed128a9a68b26fd75e71b2945a4129
Clocks-in-a-Cooler <34520271+Clocks-in-a-Cooler@users.noreply.github.com>
2018-07-18 20:42:03 +0000

    completed display_map()
    I'm not sure if this will work

--- javascript/map_panel_manager.js
+++ javascript/map_panel_manager.js
@@ -9,7 +9,15 @@ var map_panel_manager = {
     display_map: function(map) {
         var rows = map.stringify.split('\n');
 
+        var rowsleft = 0;
         
+        while (rowsleft <= rows.length) {
+            map_panel.appendChild(document.createTextNode(rows[rowsleft]));
+            
+            map_panel.appendChild(document.createElement("br"));
+            
+            rowsleft = rowsleft + 1;
+        }
     }
 }
 
