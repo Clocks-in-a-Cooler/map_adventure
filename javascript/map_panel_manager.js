09789a7d7a5539c023da8a02f07da17a22ffd71a
Clocks-in-a-Cooler <34520271+Clocks-in-a-Cooler@users.noreply.github.com>
2018-07-27 11:40:39 -0400

    added is_landmark(); updated display_map().
    MPM will not store maps. leave that to the game state manager.

--- javascript/map_panel_manager.js
+++ javascript/map_panel_manager.js
@@ -1,24 +1,57 @@
 var map_panel_manager = {
-    //for managing map_panel, only handles one map at a time
-    current_map: null,
 
     init: function() {
         this.map = new Map();
     },
 
     display_map: function(map) {
-        var rows = current_map.stringify().split('\n');
+        //clear the map
+        map_panel.innerHTML = "";
+        
+        var rows = map.stringify().split('\n');
 
         var rowsleft = 0;
         
-        while (rowsleft <= rows.length) {
-            map_panel.appendChild(document.createTextNode(rows[rowsleft]));
+        while (rowsleft < rows.length) {
+            var line = document.createElement("P");
+            var tiles = rows[rowsleft].split("");
+            var tile;
             
-            map_panel.appendChild(document.createElement("br"));
+            while (tiles.length > 0) {
+                tile = tiles.shift(); //remove a tile
+                
+                //style landmarks
+                if (this.is_landmark(tile)) {
+                    line.innerHTML += "<span style='color:black; font-weight: bold;'>" + tile + "</span>";
+                } else {
+                    line.innerHTML += tile;
+                }
+            }
             
+            var p_att = document.createAttribute("style");
+            p_att.value = "margin: 0px; display: block;";
+            line.setAttributeNode(p_att);
+            
+            map_panel.appendChild(line);
             rowsleft = rowsleft + 1;
         }
-    }
+    },
+    
+    is_landmark: function(tile) {
+        if (tile == MAP_TILES.PLAYER) {
+            return true;
+        }
+        
+        if (tile == MAP_TILES.MYSTERY) {
+            return true;
+        }
+        
+        if (tile == MAP_TILES.MARSH) {
+            return false;
+        }
+        
+        return /\w/.test(tile);
+    },
 }
 
 //alias
