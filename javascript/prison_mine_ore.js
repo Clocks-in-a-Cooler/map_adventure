4651e541090eb82ea0c90e5652fade4d3b2dfd2c
Clocks-in-a-Cooler <34520271+Clocks-in-a-Cooler@users.noreply.github.com>
2018-08-07 18:29:10 +0000

    created prison mine scene
    unfinished. add inventory and stuff later on, please.

---
+++ javascript/prison_mine_ore.js
@@ -0,0 +1,19 @@
+var p_m_ore_scene = {
+    'title': 'some ore here',
+    'scenes': {
+        'start': {
+            'text': 'there\'s some ore here.',
+            'buttons': {
+                'mine ore': {
+                    'tooltip': 'mine some of this precious stuff',
+                    'next_scene': 'end',
+                },
+                
+                'leave': {
+                    'tooltip': 'abandon this ore vein',
+                    'next_scene': 'end',
+                },
+            },
+        },
+    },
+};
