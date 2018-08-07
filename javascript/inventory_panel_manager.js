5543b9848710b2f1ae25487c2b1ea76af12b1615
clocks-in-a-cooler <>
2018-08-07 19:34:50 -0400

    made a crude inventory. still some glitches to work out. will iron out bugs!

--- javascript/inventory_panel_manager.js
+++ javascript/inventory_panel_manager.js
@@ -1,14 +1,38 @@
 var inventory_panel_manager = {
     init: function() {
-        
+
     },
-    
-    display_inventory: function() {
-        
+
+    items: {
+        //blank for now
     },
-    
-    update_inventory: function() {
-    
+
+    add_item: function(name, number) {
+        if (this.items[name]) {
+            this.items[name] += number;
+            var item = document.getElementById('item_' + name);
+            var a = item.innerHTML.split(' ');
+            item.innerHTML = a[0] + " " + this.items[name];
+        } else {
+            var inventory_item = (function () {
+                var i = document.createElement('div');
+                var c = document.createAttribute('class');
+                c.value = 'inventory_item';
+                var id = document.createAttribute('id');
+                id.value = 'item_'  + name;
+
+                i.setAttributeNode(c);
+                i.setAttributeNode(id);
+
+                return i;
+            })();
+
+            inventory_item.innerHTML = name + ": " + number;
+
+            inventory_panel.appendChild(inventory_item);
+
+            this.items[name] = number;
+        }
     },
 };
 
