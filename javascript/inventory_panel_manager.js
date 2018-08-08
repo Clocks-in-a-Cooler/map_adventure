3a526aac716ca4a75d12d4ab5b78195807ebdccf
clocks-in-a-cooler <>
2018-08-07 21:19:59 -0400

    fixed some glitches. need to optimize and add more support for other items.

--- javascript/inventory_panel_manager.js
+++ javascript/inventory_panel_manager.js
@@ -11,10 +11,16 @@ var inventory_panel_manager = {
         if (this.items[name]) {
             this.items[name] += number;
             var item = document.getElementById('item_' + name);
+
+            if (this.items[name] == 0) {
+                //remove from the inventory display
+                inventory_panel.removeChild(item);
+            }
+
             var a = item.innerHTML.split(' ');
             item.innerHTML = a[0] + " " + this.items[name];
         } else {
-            var inventory_item = (function () {
+            var inventory_item = (function() {
                 var i = document.createElement('div');
                 var c = document.createAttribute('class');
                 c.value = 'inventory_item';
@@ -34,6 +40,16 @@ var inventory_panel_manager = {
             this.items[name] = number;
         }
     },
+
+    remove_all: function(name) {
+        inventory_panel.removeChild(document.getElementById('item_' + name));
+        delete this.items[name];
+    },
+
+    clear_inventory: function() {
+        this.items = {};
+        inventory_panel.innerHTML = "";
+    }
 };
 
 //alias
